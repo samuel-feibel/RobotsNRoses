d473ed480bc96c00ce1fb4ed5522f8b05bec3fbf
yct4 <38309896+yct4@users.noreply.github.com>
2018-11-01 16:05:00 -0400

    Add files via upload
    10/31/2018 lab 4 camera code

--- OV7670_SETUP.ino
+++ OV7670_SETUP.ino
@@ -1,73 +1,86 @@
 #include <Wire.h>
 
-const byte OV7670_I2C_ADDRESS = 0x21; /*TODO: write this in hex (eg. 0xAB) */
-const byte COM7_ADDR      = 0x12; // reset all regs enable color bar test 
-const byte COM14_ADDR     = 0x3E;
-const byte CLKRC_ADDR     = 0x11; // 
-const byte PSHIFT_ADDR    = 0x1B; //
-const byte COM17_ADDR     = 0x42; //
-const byte MVFP_ADDR      = 0x1E ; //
-const byte COM9_ADDR      = 0x14; //
+const byte OV7670_I2C_ADDRESS = 0x21;
 
-const byte RESET = 0b10000000;
-//const byte COM14_SET
+const byte COM3_ADDR      = 0x0C; // enable scaling
+const byte COM7_ADDR      = 0x12; // reset all regs, enable color bar test
+const byte COM9_ADDR      = 0x14; // AGC
+const byte CLKRC_ADDR     = 0x11; // use external clock as internal clock 
+const byte COM15_ADDR     = 0x40; // scales PCLK disable internal PLL
+const byte COM17_ADDR     = 0x42; // enable color bar test 
+const byte MVFP_ADDR      = 0x1E ; // Vertical and mirror flip the output image 
+const byte PSHFT_ADDR     = 0x1B; // set camera to output the resolution specified
+const byte COM14_ADDR     = 0x3E; //const byte COM14_SET = 0x1E;
+const byte SCALING_XSC    = 0x70; // test pattern
+const byte RESET          = 0x80;
 
-byte com7_val = 0;
-byte com14_val = 0;
-byte clkrc_val = 0;
-byte pshift_val = 0;
-byte com17_val = 0;
-byte mvfp_val = 0;
-byte com9_val = 0;
+const byte COM3_SET  = 0x08; // scale enable (COM14 default set)
+const byte COM7_SET  = 0x0E; //0x0C; // RGB output and QCIF res
+const byte COM9_SET  = 0x0B; // AGC = 2x, freeze AGC
+const byte CLKRC_SET = 0xC0; // use external clock directly
+const byte COM15_SET = 0xD0; // RGB565
+const byte COM17_SET = 0x08; // color bar enable  
+const byte MVFP_SET  = 0x30; // mirror and vflip
+const byte COM14_SET = 0x08; // manual scaling enable
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);
-  
-  // TODO: READ KEY REGISTERS
-  read_key_registers();
-  delay(100);
-  // TODO: WRITE KEY REGISTERS
-  OV7670_write_register(COM14_ADDR, 0xFF); //COM14_SET
-//  OV7670_write_register(CLKRC_ADDR, CLKRC_SET);
-//  OV7670_write_register(PSHIFT_ADDR, PSHIFT_SET);
-//  OV7670_write_register(COM17_ADDR, COM17_SET);
-//  OV7670_write_register(MVFP_ADDR, MVFP_SET);
-//  OV7670_write_register(COM9_ADDR, COM9_SET);
-  
-  read_key_registers();
   OV7670_write_register(COM7_ADDR, RESET);
   read_key_registers();
+  delay(100);
+  write_key_registers();
+  read_key_registers();
+  set_color_matrix();
 }
-
 void loop(){
-    //read_key_registers();
+    
+}
+void write_key_registers(){
+    OV7670_write_register(COM7_ADDR, RESET);
+    
+    OV7670_write_register(COM3_ADDR, COM3_SET);
+    OV7670_write_register(COM7_ADDR, COM7_SET);
+    OV7670_write_register(COM9_ADDR, COM9_SET);
+    OV7670_write_register(CLKRC_ADDR, CLKRC_SET);
+    OV7670_write_register(COM15_ADDR, COM15_SET);
+    OV7670_write_register(COM17_ADDR, COM17_SET);
+    OV7670_write_register(MVFP_ADDR, MVFP_SET);
+    //OV7670_write_register(PSHFT_ADDR, PSHFT_SET);
+    OV7670_write_register(SCALING_XSC, 0xCA);
+    OV7670_write_register(COM14_ADDR, COM14_SET);
+    
 }
 
 void read_key_registers(){
-  byte com7_val;
-  com7_val = read_register_value(COM7_ADDR);
+  byte com3_val = read_register_value(COM3_ADDR);
+  Serial.print("COM3 ");
+  Serial.println(com3_val, HEX);
+  byte com7_val = read_register_value(COM7_ADDR);
   Serial.print("COM7 ");
-  Serial.println(com7_val);
-  com14_val = read_register_value(COM14_ADDR);
-  Serial.print("COM14 ");
-  Serial.println(com14_val);
-  clkrc_val = read_register_value(CLKRC_ADDR);
+  Serial.println(com7_val, HEX);
+  byte com15_val = read_register_value(COM15_ADDR);
+  Serial.print("COM15 ");
+  Serial.println(com15_val, HEX);
+  byte clkrc_val = read_register_value(CLKRC_ADDR);
   Serial.print("CLKRC ");
-  Serial.println(clkrc_val);
-  pshift_val = read_register_value(PSHIFT_ADDR);
-  Serial.print("PSHIFT ");
-  Serial.println(pshift_val);
-  com17_val = read_register_value(COM17_ADDR);
+  Serial.println(clkrc_val, HEX);
+  byte pshft_val = read_register_value(PSHFT_ADDR);
+  Serial.print("PSHFT ");
+  Serial.println(pshft_val, HEX);
+  byte com17_val = read_register_value(COM17_ADDR);
   Serial.print("COM17 ");
-  Serial.println(com17_val);
-  mvfp_val = read_register_value(MVFP_ADDR);
+  Serial.println(com17_val,HEX);
+  byte mvfp_val = read_register_value(MVFP_ADDR);
   Serial.print("MVFP ");
-  Serial.println(mvfp_val);
-  com9_val = read_register_value(COM9_ADDR);
-  Serial.print("COM9 ");
-  Serial.println(com9_val);
+  Serial.println(mvfp_val, HEX);
+  byte com14_val = read_register_value(COM14_ADDR);
+  Serial.print("COM14 ");
+  Serial.println(com14_val, HEX);
   Serial.println("");
+  byte com9_val = read_register_value(COM9_ADDR);
+  Serial.print("COM9 ");
+  Serial.println(com9_val, HEX);
 }
 
 byte read_register_value(int register_address){
